3d2e36d85e32231d889e8e322438fc9bae6a4b61
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2022-04-28 22:27:20 +0530

    update

--- init_setup.sh
+++ init_setup.sh
@@ -8,14 +8,8 @@ echo [$(date)]: "install requirements"
 pip install -r requirements.txt
 # echo [$(date)]: "export conda environment"
 # conda env export > conda.yaml
-echo [$(date)]: "create an src directory"
-mkdir src && touch src/__init__.py src/main.py
-# echo [$(date)]: "initialize git repository"
-# git init
-echo [$(date)]: "Curl .gitignore for python"
-curl https://raw.githubusercontent.com/c17hawke/Pytorch-basics/main/.gitignore > .gitignore
-# echo [$(date)]: "add env to gitignore"
-# echo "env/" >> .gitignore
+echo [$(date)]: "create an src/utils directory and files"
+mkdir -p src/utils && touch src/__init__.py src/main.py
 
 # to remove everything -
 # rm -rf env/ .gitignore conda.yaml README.md .git/
